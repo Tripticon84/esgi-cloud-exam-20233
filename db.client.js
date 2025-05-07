b344a1b171b78da9c46a93a595d7e6e032a0c49c
Tripticon84 <yannjou@free.fr>
2025-05-07 14:27:08 +0200

    add variables pour db

--- db.client.js
+++ db.client.js
@@ -2,16 +2,24 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.DB_URL,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    }
   },
-);
+)
 
 // authentication and synchronization
 sequelize.authenticate()
