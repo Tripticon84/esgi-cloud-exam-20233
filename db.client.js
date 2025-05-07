c8787e775c695bd74f288865cf9273f14c7571d4
Tripticon84 <yannjou@free.fr>
2025-05-07 14:31:32 +0200

    test

--- db.client.js
+++ db.client.js
@@ -3,23 +3,15 @@ const { Sequelize } = require('sequelize')
 // database
 const sequelize = new Sequelize(
   process.env.DB_URL,
-  process.env.DB_USER,
-  process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-    define: {
-      createdAt: 'added',
-      updatedAt: 'updated',
-    }
   },
-)
+);
 
 // authentication and synchronization
 sequelize.authenticate()
